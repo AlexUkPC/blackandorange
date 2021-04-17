ca06811fb1ef864b133a571cd43baa15f3c4dd34
AlexUkPC <alexrogna@yahoo.com>
2021-04-17 10:40:00 +0100

    typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :bandos
   devise_for :users, controllers:{registrations:'registrations'}	
-  to root 'bandos#index'
+  root 'bandos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
